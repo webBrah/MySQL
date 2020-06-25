aca9872f551433e54ce751e65df8dc5c0038f5ba
webBrah <user@Users-MacBook-Air.local>
2020-06-24 22:59:03 -0600

    Addressbook add clients

--- addressbook/add.php
+++ addressbook/add.php
@@ -1,10 +1,81 @@
 <?php
+session_start();
+
+// if user is not logged in
+if( !$_SESSION['loggedInUser'] ) {
+    
+    // send them to the login page
+    header("Location: index.php");
+}
+
+// connect to database
+include('includes/connection.php');
+
+// include functions file
+include('includes/functions.php');
+
+// if add button was submitted
+if( isset( $_POST['add'] ) ) {
+    
+    // set all variables to empty by default
+    $clientName = $clientEmail = $clientPhone = $clientAddress = $clientCompany = $clientNotes = "";
+    
+    // check to see if inputs are empty
+    // create variables with form data
+    // wrap the data with our function
+    
+    if( !$_POST["clientName"] ) {
+        $nameError = "Please enter a name <br>";
+    } else {
+        $clientName = validateFormData( $_POST["clientName"] );
+    }
+
+    if( !$_POST["clientEmail"] ) {
+        $emailError = "Please enter an email <br>";
+    } else {
+        $clientEmail = validateFormData( $_POST["clientEmail"] );
+    }
+    
+    // these inputs are not required
+    // so we'll just store whatever has been entered
+    $clientPhone    = validateFormData( $_POST["clientPhone"] );
+    $clientAddress  = validateFormData( $_POST["clientAddress"] );
+    $clientCompany  = validateFormData( $_POST["clientCompany"] );
+    $clientNotes    = validateFormData( $_POST["clientNotes"] );
+    
+    // if required fields have data
+    if( $clientName && $clientEmail ) {
+        
+        // create query
+        $query = "INSERT INTO clients (id, name, email, phone, address, company, notes, date_added) VALUES (NULL, '$clientName', '$clientEmail', '$clientPhone', '$clientAddress', '$clientCompany', '$clientNotes', CURRENT_TIMESTAMP)";
+        
+        $result = mysqli_query( $conn, $query );
+        
+        // if query was successful
+        if( $result ) {
+            
+            // refresh page with query string
+            header( "Location: clients.php?alert=success" );
+        } else {
+            
+            // something went wrong
+            echo "Error: ". $query ."<br>" . mysqli_error($conn);
+        }
+        
+    }
+    
+}
+
+// close the mysql connection
+mysqli_close($conn);
+
+
 include('includes/header.php');
 ?>
 
 <h1>Add Client</h1>
 
-<form action="clients.php" method="post" class="row">
+<form action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF'] ); ?>" method="post" class="row">
     <div class="form-group col-sm-6">
         <label for="client-name">Name *</label>
         <input type="text" class="form-control input-lg" id="client-name" name="clientName" value="">
